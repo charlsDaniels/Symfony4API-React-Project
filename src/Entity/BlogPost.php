50bf0eb6780dc53b71784356a6a6009137215c24
Menéndez, Carlos Daniel <menendez_carlos@yahoo.com.ar>
2019-08-19 10:46:18 -0300

    Added easyAdmin, API

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")7
+ * @ApiResource()
  */
 class BlogPost
 {
