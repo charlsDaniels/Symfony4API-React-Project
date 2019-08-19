16fe96761b61a41b3dabc506a8dd9fa7d59d1341
Menéndez, Carlos Daniel <menendez_carlos@yahoo.com.ar>
2019-08-19 14:55:32 -0300

    User and Comments Entities added

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,73 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
 
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        $user = $this->getReference('user_admin');
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A first post');
+        $blogPost->setPublished((new \DateTime('2019-08-08 12:00:00')));
+        $blogPost->setText('Post text');
+        $blogPost->setAuthor($user);
+        $blogPost->setSlug('a-first-post');
+
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A second post');
+        $blogPost->setPublished((new \DateTime('2019-08-08 12:00:00')));
+        $blogPost->setText('Post text');
+        $blogPost->setAuthor($user);
+        $blogPost->setSlug('a-second-post');
+
+        $manager->persist($blogPost);
+
+        $manager->flush();
+    }
+
+    public function loadComments(ObjectManager $manager)
+    {
+
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@email.com');
+        $user->setName('Charles');
+
+        $user->setPassword($this->encoder->encodePassword(
+            $user,
+            '123456'
+        ));
+
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -6,10 +6,10 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")7
  * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
  */
-class BlogPost
+class Comment
 {
     /**
      * @ORM\Id()
@@ -19,9 +19,9 @@ class BlogPost
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="text")
      */
-    private $title;
+    private $content;
 
     /**
      * @ORM\Column(type="datetime")
@@ -29,34 +29,24 @@ class BlogPost
     private $published;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $text;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="comments")
      * @ORM\JoinColumn(nullable=false)
      */
     private $author;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $slug;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getTitle(): ?string
+    public function getContent(): ?string
     {
-        return $this->title;
+        return $this->content;
     }
 
-    public function setTitle(string $title): self
+    public function setContent(string $content): self
     {
-        $this->title = $title;
+        $this->content = $content;
 
         return $this;
     }
@@ -73,30 +63,6 @@ class BlogPost
         return $this;
     }
 
-    public function getText(): ?string
-    {
-        return $this->text;
-    }
-
-    public function setText(string $text): self
-    {
-        $this->text = $text;
-
-        return $this;
-    }
-
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
-
-    public function setSlug(string $slug): self
-    {
-        $this->slug = $slug;
-
-        return $this;
-    }
-
     /**
      * @return User
      */
@@ -108,11 +74,9 @@ class BlogPost
     /**
      * @param User $author
      */
-    public function setAuthor(User $author): self
+    public function setAuthor($author): void
     {
         $this->author = $author;
-
-        return $this;
     }
 
 }
